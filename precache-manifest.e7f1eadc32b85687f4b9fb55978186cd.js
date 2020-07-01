83e24fd4dfacb45d07d3697900d9931a06ea7714
elijah <ex@gmail.com>
2020-07-01 21:24:43 +0300

    _

---
+++ precache-manifest.e7f1eadc32b85687f4b9fb55978186cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59da31326a0cfa7ad708a7b8776687a",
+    "url": "/WhiteCodeDemo/index.html"
+  },
+  {
+    "revision": "64c5239fa08807a8f79a",
+    "url": "/WhiteCodeDemo/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "3357168ea237893b16f3",
+    "url": "/WhiteCodeDemo/static/css/main.a218234f.chunk.css"
+  },
+  {
+    "revision": "64c5239fa08807a8f79a",
+    "url": "/WhiteCodeDemo/static/js/2.d3f00a6e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WhiteCodeDemo/static/js/2.d3f00a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3357168ea237893b16f3",
+    "url": "/WhiteCodeDemo/static/js/main.be299278.chunk.js"
+  },
+  {
+    "revision": "95910fa5c3b39c5346c2",
+    "url": "/WhiteCodeDemo/static/js/runtime-main.b1127760.js"
+  }
+]);
